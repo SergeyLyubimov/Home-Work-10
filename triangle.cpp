a9b91327c12a1dfb6f9c8e6ea6e078f2ca403b66
SergeyLyubimov <lyubimovvsergey@gmail.com>
2025-02-07 19:52:56 +1000

    Initial commit

---
+++ triangle.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+int main(){   
+    float xa;
+    float ya;
+    float xb;
+    float yb;
+    float xc;
+    float yc;
+
+    std::cout << "Input X for point A: ";
+    std::cin >> xa;
+
+    std::cout << "Input Y for point A: ";
+    std::cin >> ya;
+
+    std::cout << "Input X for point B: ";
+    std::cin >> xb;
+
+    std::cout << "Input Y for point B: ";
+    std::cin >> yb;
+
+    std::cout << "Input X for point C: ";
+    std::cin >> xc;
+
+    std::cout << "Input Y for point C: ";
+    std::cin >> yc;
+
+    float a = sqrt(pow((xb - xa), 2) + pow((yb - ya), 2));
+    float b = sqrt(pow((xc - xb), 2) + pow((yc - yb), 2));
+    float c = sqrt(pow((xa - xc), 2) + pow((ya - yc), 2));
+    
+    if ((xa != xb != xc && ya != yb != yc) && (a != 0 && b != 0 && c != 0)){
+        float p = a + b + c;
+        float ar = sqrt(p * (p - a) * (p - b) * (p - c));
+
+        std::cout << "Perimeter: " << p << std::endl;
+        std::cout << "Area: " << ar << std::endl;
+    } else {
+        std::cout << "Is not a triangle" << std::endl;
+    }
+}
